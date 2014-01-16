8aa025d1b6b3fe50aedd2c2e0d0ca179653be18f
Juan Pablo Bello <ingjbello@gmail.com>
2014-01-15 23:56:22 -0300

    Setting up the nav-scrollSpec.js for using nav-scroll.js file and moving the samples to firstTestSpec.js

--- spec/nav-scrollSpec.js
+++ spec/nav-scrollSpec.js
@@ -1,30 +1,7 @@
-/* 
-Here let's write the unit test for nav-scroll.js
-I am just setting up some unit tests sample which have nothing to do with the original nav-scroll.js so I can test jasmine-node working ... it works !
-*/
+var navScroll = require("../public/javascripts/nav-scroll.js");
 
-describe('jasmine-node', function(){
-
-  it('should pass', function(){
-    expect(1+2).toEqual(3);
-  });
-
-  it('shows asynchronous test', function(){
-    setTimeout(function(){
-      expect('second').toEqual('second');
-      asyncSpecDone();
-    }, 1);
-    expect('first').toEqual('first');
-    asyncSpecWait();
-  });
-
-  it('shows asynchronous test node-style', function(done){
-    setTimeout(function(){
-      expect('second').toEqual('second');
-      // If you call done() with an argument, it will fail the spec 
-      // so you can use it as a handler for many async node calls
-      done();
-    }, 1);
-    expect('first').toEqual('first');
-  });
+describe("Testing setup for nav-scroll.js - NOT real tests", function () {
+    it("navScroll created", function(){ 
+        expect(navScroll).not.toBe(null);  
+    });
 });
