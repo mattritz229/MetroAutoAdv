8ca9c7a90dd555a22b65e1aa33f3fc3d309b01f3
mattritz229 <mattritz229@yahoo.com>
2014-01-19 09:08:12 -0500

    made the background image span the whole width because it looks better now

--- public/javascripts/nav-scroll-orig.js
+++ public/javascripts/nav-scroll-orig.js
@@ -1,22 +1,16 @@
 
-//Give name to anonymous function
-var navScroll = navScrolling();
-
-function navScrolling(){
+var navScroll = (function() { 
   var scrollToAnchor = function(anchor_id) {
     var aTag = $('a[name="' + anchor_id + '"]');
     var aboveHeight = $('header').outerHeight();
     aboveHeight = aboveHeight + 20; //top padding
-    animateMenu(aboveHeight);
 
-    function animateMenu(aboveHeight) {
-      if ($(window).scrollTop() > aboveHeight){
-        var aMenuSize = mainnav1.offsetHeight;     
-      } else {
-        var aMenuSize = 0;
-      }
-      $('html,body').animate({scrollTop:aTag.offset().top - 350 + aMenuSize}, 1200);
-    };
+    if ($(window).scrollTop() > aboveHeight){
+      var aMenuSize = mainnav1.offsetHeight;     
+    } else {
+      var aMenuSize = 0;
+    }
+    $('html,body').animate({scrollTop:aTag.offset().top - 350 + aMenuSize}, 1200);
   };
 
   return {
@@ -30,14 +24,17 @@ function navScrolling(){
       });
       $('#advertisers').click(function() {
         scrollToAnchor('advertisers');
-      });
+      })
       $('#contact').click(function() {
         scrollToAnchor('contact');
-      });
+      })
 
       // navigation sticky menu bar
       var aboveHeight = $('header').outerHeight();
       aboveHeight = aboveHeight + 20; //top padding
+
+      console.log('nav scroll - aboveHeitht  ' + aboveHeight);
+      console.log('nav scroll - aboveHeitht  ');
 
       $(window).scroll(function() { 
         if ($(window).scrollTop() > aboveHeight){
@@ -46,22 +43,6 @@ function navScrolling(){
           $('nav').removeClass('navbar-fixed-top').next().css('margin-top','0');
         }
       });
-/*
-      initialize: function() {
-        _.bindAll(this, 'manageNavbarFixedTopClass');
-        $(window).scroll(this.manageNavbarFixedTopClass(aboveHeight)); 
-      }
-
-
-      function manageNavbarFixedTopClass(aboveHeight) {
-        var windowScrollTop = $(window).scrollTop();
-        if (windowScrollTop > aboveHeight){
-          $('nav').addClass('navbar-fixed-top').css('top','0').next().css('margin-top','40px');
-        } else {
-          $('nav').removeClass('navbar-fixed-top').next().css('margin-top','0');
-        }
-      };
-  */    
     }
   };
-};
+})();
